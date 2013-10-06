754784ca85668700352891a57f622576b67324fe
Robert Kenny <kenoir@gmail.com>
2013-10-06 08:01:48 +0100

    moves news service to own class

--- app.rb
+++ app.rb
@@ -1,50 +1,10 @@
 require 'sinatra'
-
 require './config'
-require './models/s3_cache'
-require './models/news_resource'
 
-class Time
-  def to_ms
-    (self.to_f * 1000.0).to_i
-  end
-end
+require './models/news_service'
 
 get '/news/:id' do
-  start_time = Time.now.to_ms
-  object_id = params[:id]
-
-  cache = S3Cache.new
-
-  freshen_if_older_than_ms = CACHE_EXPIRE_AFTER_MS
-
-  begin
-    result = cache.get(object_id)
-    last_updated = cache.last_updated(object_id).to_i
-    time_since_last_update = Time.now.to_ms - last_updated
-
-    puts "LOG: time_since_last_update #{time_since_last_update}"
-    if(time_since_last_update > freshen_if_older_than_ms)
-      puts "LOG: Cache expired for #{object_id}, refreshing ..."
-      Thread.new do
-        resource = NewsResource.new(object_id)
-        cache.put(object_id,resource.render)
-      end
-    end
-
-    puts "LOG: Writing to page at #{Time.now.to_ms}"
-    return result + "<p>(#{Time.now.to_ms}) cached: #{Time.now.to_ms - start_time}</p>"
-  rescue AWS::S3::Errors::NoSuchKey
-    puts "LOG: No cache found for #{object_id}"
-  end
-
-  resource = NewsResource.new(object_id)
-  Thread.new do
-    cache.put(object_id,resource.render)
-  end
-
-  puts "LOG: Writing to page at #{Time.now.to_ms}"
-  resource.render + "<p>(#{Time.now.to_ms})uncached: #{Time.now.to_ms - start_time}</P>"
+  news = NewsService.new
+  news.get(params[:id])
 end
-
 
---
+++ models/news_service.rb
@@ -0,0 +1,57 @@
+require './models/s3_cache'
+require './models/news_resource'
+
+class NewsService
+
+  class ::Time
+    def to_ms
+      (self.to_f * 1000.0).to_i
+    end
+  end
+
+  def initialize(cache = S3Cache.new)
+    @cache = cache
+  end
+
+  def get(id)
+    @start_time = Time.now.to_ms
+
+    begin
+      cached_data = @cache.get(id)
+
+      if(stale? id)
+        puts "LOG: Cache expired for #{id}, refreshing ..."
+        Thread.new do
+          resource = NewsResource.new(id)
+          @cache.put(id,resource.render)
+        end
+      end
+
+      return render(cached_data, "cached")
+    rescue AWS::S3::Errors::NoSuchKey
+      puts "LOG: No cache found for #{id}"
+    end
+
+    resource = NewsResource.new(id)
+    Thread.new do
+      @cache.put(id,resource.render)
+    end
+
+    render(resource.render, "uncached")
+  end
+
+  private
+  def stale?(id)
+    last_updated = @cache.last_updated(id).to_i
+    time_since_last_update = Time.now.to_ms - last_updated
+    puts "LOG: time_since_last_update #{time_since_last_update}"
+
+    time_since_last_update > CACHE_EXPIRE_AFTER_MS
+  end
+
+  def render(data,cache_status)
+    puts "LOG: Rendering at #{Time.now.to_ms}"
+    data + "<p>(#{Time.now.to_ms}) #{cache_status}: #{Time.now.to_ms - @start_time}</P>"
+  end
+
+end
